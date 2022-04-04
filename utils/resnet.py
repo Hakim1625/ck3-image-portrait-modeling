aa72f1e1759837df9ce9e2695f8943c11cb3c951
Hakim1625 <93687816+Hakim1625@users.noreply.github.com>
2022-04-03 21:53:32 -0400

    updated the branch, cleaned the dirs

---
+++ utils/resnet.py
@@ -0,0 +1,131 @@
+import torch
+import torch.nn as nn
+import pytorch_lightning as pl
+
+def init(layer):
+    torch.nn.init.xavier_uniform(layer)
+
+class block(pl.LightningModule):
+    def __init__(self, in_channels, intermediate_channels, identity_downsample=None, stride=1):
+        super(block, self).__init__()
+        self.expansion = 4
+        self.conv1 = nn.Conv2d(in_channels, intermediate_channels, kernel_size=1, stride=1, padding=0, bias=False)
+       
+        self.bn1 = nn.BatchNorm2d(intermediate_channels)
+        self.conv2 = nn.Conv2d(intermediate_channels, intermediate_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(intermediate_channels)
+        self.conv3 = nn.Conv2d(intermediate_channels, intermediate_channels * self.expansion, kernel_size=1, stride=1, padding=0, bias=False)
+        self.bn3 = nn.BatchNorm2d(intermediate_channels * self.expansion)
+        self.relu = nn.ELU()
+        self.identity_downsample = identity_downsample
+        self.stride = stride
+
+        init(self.conv1.weight)
+        init(self.conv2.weight)
+        init(self.conv3.weight)
+    
+    def forward(self, x):
+        identity = x.clone()
+
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+        x = self.conv3(x)
+        x = self.bn3(x)
+
+        if self.identity_downsample is not None:
+            identity = self.identity_downsample(identity)
+
+        x += identity
+        x = self.relu(x)
+        return x
+
+
+class ResNet(pl.LightningModule):
+    def __init__(self, block, layers, image_channels, num_features):
+        super(ResNet, self).__init__()
+        self.in_channels = 64
+        self.conv1 = nn.Conv2d(image_channels, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ELU()
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.layer1 = self._make_layer(block, layers[0], intermediate_channels=64, stride=1)
+        self.layer2 = self._make_layer(block, layers[1], intermediate_channels=128, stride=2)
+        self.layer3 = self._make_layer(block, layers[2], intermediate_channels=256, stride=2)
+        self.layer4 = self._make_layer(block, layers[3], intermediate_channels=512, stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * 4, num_features)
+        self.out = nn.Tanh()
+
+        init(self.conv1.weight)
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = torch.flatten(x, start_dim=1)
+        x = self.fc(x)
+        x = self.out(x)
+        x = x*265
+
+        return x
+
+    def _make_layer(self, block, num_residual_blocks, intermediate_channels, stride):
+        identity_downsample = None
+        layers = []
+
+        if stride != 1 or self.in_channels != intermediate_channels * 4:
+            identity_downsample = nn.Sequential(
+                nn.Conv2d(
+                    self.in_channels,
+                    intermediate_channels * 4,
+                    kernel_size=1,
+                    stride=stride,
+                    bias=False
+                ),
+                nn.BatchNorm2d(intermediate_channels * 4),
+            )
+
+        layers.append(
+            block(self.in_channels, intermediate_channels, identity_downsample, stride)
+        )
+
+        self.in_channels = intermediate_channels * 4
+
+        for _ in range(num_residual_blocks - 1):
+            layers.append(block(self.in_channels, intermediate_channels))
+
+        return nn.Sequential(*layers)
+
+
+def ResNet50(img_channel=3, num_features=1000):
+    return ResNet(block, [3, 4, 6, 3], img_channel, num_features)
+
+
+def ResNet101(img_channel=3, num_features=1000):
+    return ResNet(block, [3, 4, 23, 3], img_channel, num_features)
+
+
+def ResNet152(img_channel=3, num_features=1000):
+    return ResNet(block, [3, 8, 36, 3], img_channel, num_features)
+
+
+def main():
+    model = ResNet50(img_channel=6, num_features=101)
+    y = model(torch.randn(1, 6, 224, 224)).to('cuda')
+    print(y)
+
+
+if __name__ == "__main__":
+    main()
