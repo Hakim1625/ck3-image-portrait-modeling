e4547ffffe26eb6af53c808e88f05b52d32da0a1
Hakim1625 <hakim@pop-os.localdomain>
2022-06-08 15:23:05 -0400

    compress new_folder at end of execution

--- process.py
+++ process.py
@@ -1,5 +1,6 @@
 from utils.data_processing import torchvision_dataset_align
 import zipfile
+import shutil
 
 with zipfile.ZipFile('/datastores/ck3-portraits/portraits.zip', 'r') as zip_ref:
     zip_ref.extractall('./')
@@ -7,3 +8,6 @@ with zipfile.ZipFile('/datastores/ck3-portraits/portraits.zip', 'r') as zip_ref:
 dataset = torchvision_dataset_align(directory = "./portraits")
 dataset.num_workers = 4
 dataset.process_and_save
+
+
+shutil.make_archive('./', 'zip', './portraits_aligned')
