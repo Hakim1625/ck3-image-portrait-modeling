cc35e3e6b064c97fdafcf43baa1e3f576d84eeca
Hakim1625 <hakim@pop-os.localdomain>
2022-06-09 04:13:52 -0400

    small path changes

--- utils/data_processing.py
+++ utils/data_processing.py
@@ -172,6 +172,6 @@ class torchvision_dataset_align(dataset_align):
         self.pbar.close()
         print('Dataset aligned!')
 
-        shutil.make_archive('./', 'zip', self.directory)
+        shutil.make_archive(self.directory, 'zip', './')
         print('Datset compressed!')
 
